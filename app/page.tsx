260b78072f5bc4a6ad7a85baeeb7437f8efd2a3d
stivex001 <stephenadeyemo@gmail.com>
2024-05-13 10:03:36 +0100

    fix:typo fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-<main className="bg-black min-h-screen text-white ">
-  <h1>welcome to my portfolio</h1>
-</main>
+    <main className="bg-black min-h-screen text-white ">
+      <h1>welcome to my portfolio</h1>
+    </main>
   );
 }
