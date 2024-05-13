abce7bd5623753ad2b904b7f6b229f28dcb7b7e9
stivex001 <stephenadeyemo@gmail.com>
2024-05-13 10:01:18 +0100

    feat:first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<main className="bg-black min-h-screen text-white ">
+  <h1>welcome to my portfolio</h1>
+</main>
+  );
+}
