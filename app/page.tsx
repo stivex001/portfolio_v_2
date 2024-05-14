7948677158980609d3823703076eef76afadc3a0
stivex001 <stephenadeyemo@gmail.com>
2024-05-14 17:14:05 +0100

    feat:bg changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="bg-black min-h-screen text-white ">
+    <main className="bg-[#251C31] min-h-screen text-white ">
       <h1 className="text-3xl text-center">welcome to my portfolio</h1>
     </main>
   );
