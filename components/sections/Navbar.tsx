e6f10f7d8aae8f3dddf2375467ddd02eb1e52d18
stivex001 <stephenadeyemo@gmail.com>
2024-05-16 22:43:07 +0100

    feat:added toggle bar state to the navbar

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import { Menu } from "lucide-react";
@@ -25,6 +27,8 @@ const links = [
 ];
 
 export const Navbar = (props: Props) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="bg-secondary h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
       <Link
@@ -37,22 +41,27 @@ export const Navbar = (props: Props) => {
         </span>
       </Link>
       {/* menu */}
+
       <div>
-        <button>
+        <button
+          className="z-50 relative"
+          onClick={() => setOpen((prev) => !prev)}
+        >
           <Menu size={40} />
         </button>
       </div>
-
-      <div className="absolute top-0 left-0 w-screen h-screen bg-primary text-grayish flex flex-col items-center justify-center gap-8 text-4xl">
-        {links?.map((link) => (
-          <Link href={link.url} key={link.title}>
-            {link.title}{" "}
-          </Link>
-        ))}
-        <Button className="bg-grayish text-primary hover:bg-grayish/60">
-          Download Resume
-        </Button>
-      </div>
+      {open && (
+        <div className="absolute top-0 left-0 w-screen h-screen bg-primary text-grayish flex flex-col items-center justify-center gap-8 text-4xl">
+          {links?.map((link) => (
+            <Link href={link.url} key={link.title}>
+              {link.title}{" "}
+            </Link>
+          ))}
+          <Button className="bg-grayish text-primary hover:bg-grayish/60">
+            Download Resume
+          </Button>
+        </div>
+      )}
 
       {/* <div className="flex items-center gap-12">
         <ul className="flex items-center gap-6">
