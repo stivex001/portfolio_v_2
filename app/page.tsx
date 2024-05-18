c02ac6bad815a722d161492dc0a10f159648f6f9
stivex001 <stephenadeyemo@gmail.com>
2024-05-18 02:03:31 +0100

    fix: styling fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import me from "@/components/images/me.png";
 import { MapIcon } from "@/components/icons/MapIcon";
+import { Facebook, GithubIcon, Linkedin, TwitterIcon } from "lucide-react";
 
 export default function Home() {
   return (
@@ -9,7 +10,9 @@ export default function Home() {
         <Image src={me} alt="bai" fill className="object-contain " />
       </div>
       <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
-        <h1 className="text-grayish w-full font-bold text-4xl md:6xl">Hi, I&apos;m Stephen 👋</h1>
+        <h1 className="text-grayish w-full font-bold text-4xl md:6xl">
+          Hi, I&apos;m Stephen 👋
+        </h1>
         <span className="md:text-xl">
           Specializing in front-end development, I excel in leveraging
           technologies like React, Next.js, and server-side rendering to craft
@@ -28,6 +31,36 @@ export default function Home() {
             <div className="w-2 h-2 rounded-full bg-[#10B981]" />
             <span>Available for new projects</span>
           </div>
+        </div>
+        <div className="flex items-center gap-2.5 w-full">
+          <a
+            href="https://github.com/stivex001"
+            target="_blank"
+            className="w-10 h-10 rounded-full bg-secondary flex items-center justify-center"
+          >
+            <GithubIcon />
+          </a>
+          <a
+            href="https://github.com/stivex001"
+            target="_blank"
+            className="w-10 h-10 rounded-full bg-secondary flex items-center justify-center"
+          >
+            <TwitterIcon />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/adeyemostephen/"
+            target="_blank"
+            className="w-10 h-10 rounded-full bg-secondary flex items-center justify-center"
+          >
+            <Linkedin />
+          </a>
+          <a
+            href="https://www.facebook.com/adeyemo.oladunjoyestephen"
+            target="_blank"
+            className="w-10 h-10 rounded-full bg-secondary flex items-center justify-center"
+          >
+            <Facebook  />
+          </a>
         </div>
       </div>
     </main>
