4c2071e9a63d256f3d54adf79f19ca91571a7056
stivex001 <stephenadeyemo@gamil.com>
2024-06-09 00:49:42 +0100

    feat:implementing experience page

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+"use client";
 
-type Props = {}
+import React from "react";
+import { motion } from "framer-motion";
+import { MyExperience } from "@/components/About/Experience";
+
+type Props = {};
 
 const Experience = (props: Props) => {
   return (
-    <div>Experience</div>
-  )
-}
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="flex">
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
+          <MyExperience />
+        </div>
+        <div></div>
+      </div>
+    </motion.div>
+  );
+};
 
-export default Experience
+export default Experience;
