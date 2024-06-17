1ebd7c98ad94aff73e42c15da8dbdf6b31fab336
stivex001 <stephenadeyemo@gmail.com>
2024-06-17 11:03:43 +0100

    feat:added animation to the svg component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useRef } from "react";
+import { motion, useScroll } from "framer-motion";
 import { Biography } from "@/components/About/Biography";
 import { Skills } from "@/components/About/Skills";
 import { MyExperience } from "@/components/About/Experience";
@@ -10,6 +10,11 @@ import { SvgAnimation } from "@/components/About/SvgAnimation";
 type Props = {};
 
 const About = (props: Props) => {
+
+  const containerRef = useRef(null);
+
+  const {scrollYProgress} = useScroll({container:containerRef})
+
   return (
     <motion.div
       className="h-full"
@@ -17,13 +22,13 @@ const About = (props: Props) => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full overflow-scroll lg:flex overflow-x-hidden">
+      <div className="h-full overflow-scroll lg:flex overflow-x-hidden" ref={containerRef}>
         <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:1/2">
           <Biography />
           <Skills />
         </div>
         <div className="hidden lg:block w-1/3 xl:1/2 sticky top-0 z-30">
-          <SvgAnimation />
+          <SvgAnimation scrollYProgress={scrollYProgress} />
         </div>
       </div>
     </motion.div>
--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useRef } from "react";
+import { motion, useScroll } from "framer-motion";
 import { MyExperience } from "@/components/About/Experience";
 import { SvgAnimation } from "@/components/About/SvgAnimation";
 
 type Props = {};
 
 const Experience = (props: Props) => {
+
+  const containerRef = useRef(null)
+
+  const {scrollYProgress} = useScroll({container:containerRef})
+
   return (
     <motion.div
       className="h-full"
@@ -15,12 +20,12 @@ const Experience = (props: Props) => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full overflow-scroll lg:flex overflow-x-hidden">
+      <div className="h-full overflow-scroll lg:flex overflow-x-hidden" ref={containerRef}>
         <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:1/2">
           <MyExperience />
         </div>
         <div className="hidden lg:block w-1/3 xl:1/2 sticky top-0 z-30">
-          <SvgAnimation />
+          <SvgAnimation scrollYProgress={scrollYProgress}/>
         </div>
       </div>
     </motion.div>
