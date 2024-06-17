700e2b6ec5ff72045ec33885922319cf4a98393a
stivex001 <stephenadeyemo@gmail.com>
2024-06-18 00:27:47 +0100

    feat:implementing my project page

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+"use client";
+import React, { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 
-type Props = {}
+type Props = {};
 
 const Project = (props: Props) => {
-  return (
-    <div>Project</div>
-  )
-}
+  const containerRef = useRef(null);
 
-export default Project
+  const { scrollYProgress } = useScroll({ container: containerRef });
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "100%"])
+
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-[600vh] relative" ref={containerRef}>
+        <div className="w-screen h-screen flex items-center justify-center text-8xl text-center"> My Works</div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Project;
