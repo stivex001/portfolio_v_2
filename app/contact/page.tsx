f37c5e6dc74069cb505218e5228986d956630dba
stivex001 <stephenadeyemo@gmail.com>
2024-06-20 00:34:49 +0100

    feat:implemented email with emai;.js

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,14 +1,50 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
 
 type Props = {};
 
 const Contact = (props: Props) => {
-  const [success, setSuccess] = useState(false)
-  const [error, setError] = useState(false)
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
   const text = "Say Hello";
+
+  const form = useRef<HTMLFormElement | null>(null);
+
+  const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(false);
+    setSuccess(false);
+
+    if (!form.current) {
+      console.error("Form ref is not assigned.");
+      return;
+    }
+
+
+    emailjs
+      .sendForm(
+        `${process.env.NEXT_PUBLIC_SERVICE_ID}`,
+        `${process.env.NEXT_PUBLIC_TEMPLATE_ID}`,
+        form.current,
+        {
+          publicKey: `${process.env.NEXT_PUBLIC_PUBLIC_KEY}`,
+        }
+      )
+      .then(
+        (result) => {
+          setSuccess(true);
+          form?.current?.reset();
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          setError(true);
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
 
   return (
     <motion.div
@@ -37,15 +73,37 @@ const Contact = (props: Props) => {
             😊
           </div>
         </div>
-        <form className="h-1/2 lg:h-full lg:w-1/2 bg-transparent rounded-xl text-xl flex flex-col gap-8 justify-center p-24">
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="h-1/2 lg:h-full lg:w-1/2 bg-transparent rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
+        >
           <span>Dear Stephen,</span>
-          <textarea rows={6} className="bg-transparent border-b-2 border-b-white outline-none resize-none"/>
+          <textarea
+            rows={6}
+            name="user_message"
+            className="bg-transparent border-b-2 border-b-white outline-none resize-none"
+          />
           <span>My mail address is:</span>
-          <input type="email" className="bg-transparent border-b-2 border-b-white outline-none "/>
+          <input
+            name="user_email"
+            type="email"
+            className="bg-transparent border-b-2 border-b-white outline-none "
+          />
           <span>Regards</span>
-          <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">Send</button>
-          {success && <span className="text-green-600 font-semibold">Your message has been sent successfully</span>}
-          {error && <span className="text-red-600 font-semibold">Something went wrong!</span>}
+          <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">
+            Send
+          </button>
+          {success && (
+            <span className="text-green-600 font-semibold">
+              Your message has been sent successfully
+            </span>
+          )}
+          {error && (
+            <span className="text-red-600 font-semibold">
+              Something went wrong!
+            </span>
+          )}
         </form>
       </div>
     </motion.div>
