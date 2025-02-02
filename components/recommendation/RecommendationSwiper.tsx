86e82476bde31a0f4143cabd3aab0a8cbce673f7
stivex001 <stephenadeyemo@gmail.com>
2025-02-02 14:13:00 +0100

    feat:working on the recommendation section

--- components/recommendation/RecommendationSwiper.tsx
+++ components/recommendation/RecommendationSwiper.tsx
@@ -7,6 +7,11 @@ import { a, useSpring, useSpringRef, useTransition } from "@react-spring/web";
 import { useSpring as useThreeSpring } from "@react-spring/three";
 import { percentToRadians } from "@/utils/convertion";
 import EarthThree from "../react-three/EarthThree";
+import LocationIcon from "../svg/abstract/LocationIcon";
+import CustomTooltip from "../clickable/CustomTooltip";
+import PrevIcon from "../svg/icons/PrevIcon";
+import NextIcon from "../svg/icons/NextIcon";
+import RecommendationCard from "./RecommendationCard";
 
 type Props = {};
 
@@ -74,6 +79,18 @@ export const RecommendationSwiper = ({
     recommendations,
   ]);
 
+  const nextRecommendation = () => {
+    if (recommendationIndex < recommendations.length) {
+      setRecommedationIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  const previousRecommendation = () => {
+    if (recommendationIndex > 0) {
+      setRecommedationIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
   return (
     <div
       className="flex flex-col gap-[36px] md:flex-row justify-between recommendations-swiper my-8 md:my-2 semi-lg:mx-12 relative"
@@ -85,8 +102,113 @@ export const RecommendationSwiper = ({
         ref={observedRef}
         style={earthViewedSpring}
       >
-        <EarthThree rotationSpring={earthRotationSpring} setLoaded={setEarthLoaded} />
+        <EarthThree
+          rotationSpring={earthRotationSpring}
+          setLoaded={setEarthLoaded}
+        />
+        {earthLoaded ? (
+          <div
+            className={`
+            group/earth-pointer
+            ${earthRotating ? "is-disabled" : "is-active"}
+            absolute top-1/2 left-1/2
+            -translate-x-1/2 -translate-y-1/2
+          `}
+          >
+            <div
+              className="relative isolate"
+              data-tooltip-id="active-recommendation-location"
+              aria-describedby="active-recommendation-location"
+            >
+              <LocationIcon />
+              <div
+                className={`
+              absolute -z-10
+              h-[14px] aspect-square rounded-[50%]
+              bg-grey-9 dark:bg-black
+              ring-2 ring-blue-100 dark:ring-blue-d-200
+              top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+              ${earthRotating ? "scale-0" : "scale-100"}
+              transition duration-300 pointer-events-none
+            `}
+              />
+              <div
+                className={`
+                absolute -z-20
+                h-[12px] aspect-square rounded-[50%]
+                bg-black/20 dark:bg-white/30
+                top-1/2 left-1/2 opacity-0 pointer-events-none
+                ${earthRotating ? "" : "expanding-scale"}
+              `}
+              />
+            </div>
+            {!earthRotating ? (
+              <CustomTooltip id="active-recommendation-location">
+                <span>{recommendations[recommendationIndex].location}</span>
+              </CustomTooltip>
+            ) : null}
+          </div>
+        ) : null}
       </a.div>
+      <div className="card-container min-h-[400px] grid place-items-center relative mr-0 md:mr-[24px] semi-lg:mr-0 px-[12px] md:px-0 isolate">
+        {cardTransition((style, cardIndex) => (
+          <RecommendationCard
+            recommendation={recommendations[cardIndex]}
+            recommendationIndex={cardIndex}
+            cardTransition={style}
+          />
+        ))}
+        <button
+          className={`
+            z-10 group/icon 
+            w-[48px] aspect-square rounded-[50%] grid place-items-center
+            absolute top-1/2 left-0 -translate-y-1/2 -translate-x-[12px] md:-translate-x-[36px] semi-lg:-translate-x-[60px] backdrop-blur-sm
+
+            ring-1 ring-grey-b dark:ring-grey-4
+            bg-grey-d/50 dark:bg-grey-3/50
+            
+            hover:ring-grey-9 dark:hover:ring-grey-5
+            hover:bg-grey-d/75 dark:hover:bg-grey-5/50
+
+            disabled:pointer-events-none
+            disabled:opacity-0 disabled:scale-75
+            transition
+          `}
+          onClick={previousRecommendation}
+          disabled={recommendationIndex === 0}
+          aria-hidden={recommendationIndex === 0}
+          name={`previous recommendation`}
+          aria-label={`previous recommendation`}
+          aria-controls="recommendation-carousel"
+        >
+          <PrevIcon />
+        </button>
+        <button
+          className={`
+            z-10 group/icon 
+            w-[48px] aspect-square rounded-[50%] grid place-items-center 
+            absolute top-1/2 right-0 -translate-y-1/2 translate-x-[12px] md:translate-x-[36px] semi-lg:translate-x-[60px] backdrop-blur-[2px]
+
+            ring-1 ring-grey-b dark:ring-grey-4
+            bg-grey-d/50 dark:bg-grey-3/50
+            
+            hover:ring-grey-9 dark:hover:ring-grey-5
+            hover:bg-grey-d/75 dark:hover:bg-grey-5/50
+
+            disabled:pointer-events-none
+            disabled:opacity-0 disabled:scale-75
+            transition
+          `}
+          onClick={nextRecommendation}
+          disabled={recommendationIndex === recommendations.length - 1}
+          aria-hidden={recommendationIndex === recommendations.length - 1}
+          name={`next recommendation`}
+          aria-label={`next recommendation`}
+          aria-controls="recommendation-carousel"
+        >
+          <NextIcon />
+        </button>
+      </div>
     </div>
   );
 };
